f9c6114a6a219227916eab5f397768f0806a28b8
jasondemps1 <jasondemps1@gmail.com>
2020-05-30 13:48:28 -0400

    Oh man, it works. This is amazing. Just make sure you have qemu installed!

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+// Disable std lib
+#![no_std]
+// Tell Rust we dont want the normal entry point (Rust's real crt0 entrypoint is fn start)
+#![no_main]
+
+// Define our own panic_handler, std lib provides one, but we have to roll our own now!
+use core::panic::PanicInfo;
+
+// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+// Disable name mangling (required, linker needs to know where our fn is!).
+// This is our entry point! We use never "!", as the entry point isn't called by a function, it's invoked directly by the OS/bootloader.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // VGA Test!!!!!!!!
+    // VGA Text Buffer is at 0xb8000
+    // Cast the address to a raw pointer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan
+        }
+    }
+
+    loop {}
+}
