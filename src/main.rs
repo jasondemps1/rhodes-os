1d42aed070567164798a3adf5b6086ddee0761a4
jasondemps1 <jasondemps1@gmail.com>
2020-05-30 17:08:22 -0400

    Writing hello world!

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,7 @@ static HELLO: &[u8] = b"Hello World!";
 // This is our entry point! We use never "!", as the entry point isn't called by a function, it's invoked directly by the OS/bootloader.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // VGA Test!!!!!!!!
-    // VGA Text Buffer is at 0xb8000 (memory-mapped I/O)
-    // Cast the address to a raw pointer
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
